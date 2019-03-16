6f3c6bd2a0b8641849f48039f0661ab993a427bc
grey <fairingrey@gmail.com>
2019-03-16 14:44:46 -0700

    another easy problem

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ mod n0349_intersection_of_two_arrays;
 mod n0350_intersection_of_two_arrays_ii;
 mod n0709_to_lower_case;
 mod n0771_jewels_and_stones;
+mod n1005_maximize_sum_of_array_after_k_negations;
---
+++ src/n1005_maximize_sum_of_array_after_k_negations.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn largest_sum_after_k_negations(mut a: Vec<i32>, k: i32) -> i32 {
+        a.sort_unstable();
+        for _ in 0..k {
+            a[0] *= -1;
+            a.sort_unstable();
+        }
+        a.iter().fold(0, |acc, x| acc + x)
+    }
+}

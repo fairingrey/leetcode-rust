b938167494833d3e6a024a52c108e9cdda4c629f
grey <fairingrey@gmail.com>
2019-04-13 00:56:24 -0700

    add solution to outside problem

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,5 @@ mod n0350_intersection_of_two_arrays_ii;
 mod n0709_to_lower_case;
 mod n0771_jewels_and_stones;
 mod n1005_maximize_sum_of_array_after_k_negations;
+
+mod other_maximum_sum_nonadjacents;
---
+++ src/other_maximum_sum_nonadjacents.rs
@@ -0,0 +1,53 @@
+//    Not a leetcode problem (yet?), hence the 'other'. I'm just including it here for kicks
+
+//    This problem was asked by Airbnb.
+//
+//    Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+//
+//    For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+//
+//    Follow-up: Can you do this in O(N) time and constant space?
+
+pub struct Solution {}
+impl Solution {
+    pub fn maximum_sum_nonadjacents(nums: Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+
+        let mut sum_include_prev = nums[0];
+        let mut sum_exclude_prev = 0;
+
+        let mut iterator = nums.iter().skip(1);
+
+        for num in iterator {
+            if sum_exclude_prev + *num >= sum_include_prev {
+                sum_exclude_prev += *num;
+            } else {
+                sum_exclude_prev = sum_include_prev;
+            }
+            std::mem::swap(&mut sum_exclude_prev, &mut sum_include_prev);
+        }
+
+        if sum_include_prev > sum_exclude_prev {
+            sum_include_prev
+        } else {
+            sum_exclude_prev
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn other_nonadjacents_1() {
+        assert_eq!(13, Solution::maximum_sum_nonadjacents(vec![2, 4, 6, 2, 5]));
+    }
+
+    #[test]
+    fn other_nonadjacents_2() {
+        assert_eq!(10, Solution::maximum_sum_nonadjacents(vec![5, 1, 1, 5]));
+    }
+}

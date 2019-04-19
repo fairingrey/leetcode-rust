7708c392628b242e1d44da78d20ed9c03764391b
grey <fairingrey@gmail.com>
2019-04-18 23:43:36 -0700

    add monte carlo method of estimating pi (not leetcode, but hey)

--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,5 @@ mod n0709_to_lower_case;
 mod n0771_jewels_and_stones;
 mod n1005_maximize_sum_of_array_after_k_negations;
 
+mod other_estimate_pi_monte_carlo;
 mod other_maximum_sum_nonadjacents;
---
+++ src/other_estimate_pi_monte_carlo.rs
@@ -0,0 +1,29 @@
+//---
+//Wednesday, April 17th, 2019
+//---
+//Good morning! Here's your coding interview problem for today.
+//
+//This problem was asked by Google.
+//
+//The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+//
+//Hint: The basic equation of a circle is x2 + y2 = r2.
+use rand::Rng;
+
+pub struct Solution {}
+impl Solution {
+    pub fn estimate_pi(runs: usize) -> f64 {
+        let mut rng = rand::thread_rng();
+
+        (0..runs).fold(0, |acc, _| {
+            let (x, y) = (rng.gen::<f64>(), rng.gen::<f64>());
+            if x.powi(2) + y.powi(2) <= 1. {
+                acc + 1
+            } else {
+                acc
+            }
+        }) as f64
+            * 4.
+            / (runs as f64)
+    }
+}

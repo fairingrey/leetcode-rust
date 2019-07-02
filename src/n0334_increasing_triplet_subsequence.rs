e93ebf6e120fb6073b049d7f97005f227d7f7c56
grey <fairingrey@gmail.com>
2019-07-02 11:19:02 -0700

    add alternate solution to n0334

--- src/n0334_increasing_triplet_subsequence.rs
+++ src/n0334_increasing_triplet_subsequence.rs
@@ -18,4 +18,24 @@ impl Solution {
 
         false
     }
+
+    // This is an alternate solution that works (and also generalizes the length of the subsequence
+    pub fn increasing_subsequence_generalized(nums: Vec<i32>) -> bool {
+        let subseq_length = 3;
+
+        let mut subseq_nums = vec![std::i32::MAX; subseq_length - 1];
+
+        for v in nums.into_iter() {
+            for subseq_v in &mut subseq_nums {
+                if v <= *subseq_v {
+                    *subseq_v = v;
+                    break;
+                }
+            }
+            if v > *subseq_nums.last().unwrap() {
+                return true;
+            }
+        }
+        false
+    }
 }
